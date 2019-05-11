2037194b8c3b36df1321838149eeb33124b662d7
queerpassenger <queerpassenger@gmail.com>
2019-05-11 17:29:28 +0530

    ClientIdWith

--- auth.js
+++ auth.js
@@ -9,8 +9,10 @@ module.exports = function (passport) {
         done(null, user);
     });
     passport.use(new GoogleStrategy({
-        clientID: "94203692968-v8821qla15ijtmv0n2qq39al9vacrgng.apps.googleusercontent.com",
-        clientSecret: "T8BYCxeJG7WzvvrwQxuPycPA",
+        // clientID: "94203692968-v8821qla15ijtmv0n2qq39al9vacrgng.apps.googleusercontent.com",
+        // clientSecret: "T8BYCxeJG7WzvvrwQxuPycPA",
+        clientID:'724697680271-hcsr4o7jv6s5vfko5toou1ld6tusnouv.apps.googleusercontent.com',
+        clientSecret:'TCHe-hkidCkOs5gi9UL5_HIO',
         callbackURL: '/auth/google/callback'
     }, (token, refreshToken, profile, done) => {
         return done(null, {
